a9eb21bae0cdbb59138aca6defac5fd1476b661d
jhlee93 <bigjh64@gmail.com>
2019-09-16 14:49:20 +0900

    test rotated license plate

---
+++ to_gray.py
@@ -0,0 +1,22 @@
+import os
+import cv2
+
+def main():
+    img_folder = "./data/plate_rotate/"
+
+    img_list = os.listdir(img_folder)
+
+    for i in img_list:
+        img_path = img_folder + i
+
+        img = cv2.imread(img_path, 0)
+
+        cv2.imwrite(img_path, img)
+
+        gray_3ch = cv2.imread(img_path)
+        cv2.imwrite(img_path, gray_3ch)
+
+    
+
+if __name__ == '__main__':
+    main()
